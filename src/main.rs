ce3270de9cf1a08de899fbabdc6f9cffa199f6c1
scttpr <charles.capelli@qonfucius.team>
2022-07-23 23:08:32 +0200

    feat: set example to run on clone

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,12 @@ use std::fs;
 // Data from RC example, please update according to your intercepted data
 const HASH: &str = "c4c9334bac560ecc979e58001b3e22fb";
 const TIMESTAMP: &str = "<1896.697170952@dbc.mtview.ca.us>";
-const DICTIONARY_PATH: &str = "/your_favorite_wordlist.txt";
+const DICTIONARY_PATH: &str = "./wordlist_example.txt";
 
 fn main() {
-    let raw_dictionary =
-        fs::read_to_string(DICTIONARY_PATH).expect("Unable to read dictionary file");
+    let dictionary = fs::read_to_string(DICTIONARY_PATH).expect("Unable to read dictionary file");
 
-    let dictionary = raw_dictionary.lines();
-
-    for password in dictionary {
+    for password in dictionary.lines() {
         let secret = format!("{}{}", TIMESTAMP, password);
         println!("Testing secret {}", secret);
 
